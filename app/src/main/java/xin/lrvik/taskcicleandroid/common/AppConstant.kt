eea8ab857e80b4c78ca1d9916811af2731f254c5
豢涵 <huanhanfu@126.com>
2019-01-02 19:43:45 +0800

    增加轮播和分类

--- app/src/main/java/xin/lrvik/taskcicleandroid/common/AppConstant.kt
+++ app/src/main/java/xin/lrvik/taskcicleandroid/common/AppConstant.kt
@@ -4,3 +4,44 @@ package xin.lrvik.taskcicleandroid.common
  * Author by 豢涵, Email huanhanfu@126.com, Date on 2018/12/25.
  *
  */
+
+const val HOME_BANNER_ONE = "https://fuss10.elemecdn.com/7/90/8012dcb83434050f0363316f24809jpeg.jpeg?imageMogr/format/webp/thumbnail/568x/"
+const val HOME_BANNER_TWO = "https://fuss10.elemecdn.com/d/f5/456ea6285819f7932d8606bffeb35jpeg.jpeg?imageMogr/format/webp/thumbnail/568x/"
+
+
+const val HOME_TYPE_IMG_1 = "http://p1.meituan.net/jungle/8b5e1702d4145ccf058ba5fb31008c5310912.png"
+const val HOME_TYPE_IMG_2 = "http://p0.meituan.net/jungle/45ff2f098a20a77122bff8385192f0ec10547.png"
+const val HOME_TYPE_IMG_3 = "http://p1.meituan.net/jungle/12a9834827909fa55f54bce96e67470711250.png"
+const val HOME_TYPE_IMG_4 = "http://p1.meituan.net/jungle/da9181f93dd2e11c5d74cf685470408f10016.png"
+const val HOME_TYPE_IMG_5 = "http://p1.meituan.net/jungle/83bfbcf07221cb1ccbcd9114e81ec7ac9209.png"
+const val HOME_TYPE_IMG_6 = "http://p1.meituan.net/jungle/a2a306c5467aba7fac8d7410d161f8db9364.png"
+const val HOME_TYPE_IMG_7 = "http://p0.meituan.net/jungle/2eee7747fd143249b100b9fa5ee5e31d9873.png"
+const val HOME_TYPE_IMG_8 = "http://p0.meituan.net/jungle/2ec76f8f4f21a4ec163adc7fccfa1a399428.png"
+const val HOME_TYPE_IMG_9 = "http://p0.meituan.net/jungle/2ec76f8f4f21a4ec163adc7fccfa1a399428.png"
+const val HOME_TYPE_IMG_10 = "http://p0.meituan.net/jungle/2eee7747fd143249b100b9fa5ee5e31d9873.png"
+const val HOME_TYPE_IMG_11 = "http://p1.meituan.net/jungle/a2a306c5467aba7fac8d7410d161f8db9364.png"
+const val HOME_TYPE_IMG_12 = "http://p1.meituan.net/jungle/83bfbcf07221cb1ccbcd9114e81ec7ac9209.png"
+const val HOME_TYPE_IMG_13 = "http://p1.meituan.net/jungle/da9181f93dd2e11c5d74cf685470408f10016.png"
+const val HOME_TYPE_IMG_14 = "http://p1.meituan.net/jungle/12a9834827909fa55f54bce96e67470711250.png"
+const val HOME_TYPE_IMG_15 = "http://p0.meituan.net/jungle/45ff2f098a20a77122bff8385192f0ec10547.png"
+const val HOME_TYPE_IMG_16 = "http://p1.meituan.net/jungle/8b5e1702d4145ccf058ba5fb31008c5310912.png"
+
+const val HOME_TYPE_TITLE_1 = "美食"
+const val HOME_TYPE_TITLE_2 = "超市"
+const val HOME_TYPE_TITLE_3 = "生鲜"
+const val HOME_TYPE_TITLE_4 = "专送"
+const val HOME_TYPE_TITLE_5 = "简餐"
+const val HOME_TYPE_TITLE_6 = "下午茶"
+const val HOME_TYPE_TITLE_7 = "家常菜"
+const val HOME_TYPE_TITLE_8 = "小吃馆"
+const val HOME_TYPE_TITLE_9 = "小吃馆"
+const val HOME_TYPE_TITLE_10 = "家常菜"
+const val HOME_TYPE_TITLE_11 = "下午茶"
+const val HOME_TYPE_TITLE_12 = "简餐"
+const val HOME_TYPE_TITLE_13 = "专送"
+const val HOME_TYPE_TITLE_14 = "生鲜"
+const val HOME_TYPE_TITLE_15 = "超市"
+const val HOME_TYPE_TITLE_16 = "美食"
+
+
+
--- app/src/main/java/xin/lrvik/taskcicleandroid/ui/fragment/HomeFragment.kt
+++ app/src/main/java/xin/lrvik/taskcicleandroid/ui/fragment/HomeFragment.kt
@@ -4,8 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_home.*
+import xin.lrvik.easybanner.Transformer
+import xin.lrvik.easybanner.adapter.viewpager.EasyImageAdapter
+import xin.lrvik.easybanner.adapter.viewpager.EasyTypeItemAdapter
+import xin.lrvik.easybanner.dto.TypeItem
 import xin.lrvik.taskcicleandroid.R
+import xin.lrvik.taskcicleandroid.baselibrary.ext.loadUrl
 import xin.lrvik.taskcicleandroid.baselibrary.ui.fragment.BaseFragment
+import xin.lrvik.taskcicleandroid.common.*
 
 
 class HomeFragment : BaseFragment() {
@@ -14,6 +23,40 @@ class HomeFragment : BaseFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        mEvpBanner.setBannerAnimation(Transformer.Accordion)
+                .setIndicator(mEdiBanner)
+                .setAdapter(object : EasyImageAdapter<String>() {
+                    override fun convert(view: ImageView, data: String) {
+                        view.loadUrl(data)
+                    }
+
+                }).data = listOf(HOME_BANNER_ONE, HOME_BANNER_TWO)
+
+
+        mEvpType.setIndicator(mEdiType)
+                .setAdapter(object : EasyTypeItemAdapter(10, 5) {
+                    override fun convert(imageView: ImageView, textView: TextView, data: TypeItem) {
+                        imageView.loadUrl(data.imgUrl)
+                        textView.textSize = 12f
+                        textView.text = data.title
+                    }
+                }).data = listOf(TypeItem(HOME_TYPE_IMG_1, HOME_TYPE_TITLE_1),
+                TypeItem(HOME_TYPE_IMG_2, HOME_TYPE_TITLE_2),
+                TypeItem(HOME_TYPE_IMG_3, HOME_TYPE_TITLE_3),
+                TypeItem(HOME_TYPE_IMG_4, HOME_TYPE_TITLE_4),
+                TypeItem(HOME_TYPE_IMG_5, HOME_TYPE_TITLE_5),
+                TypeItem(HOME_TYPE_IMG_6, HOME_TYPE_TITLE_6),
+                TypeItem(HOME_TYPE_IMG_7, HOME_TYPE_TITLE_7),
+                TypeItem(HOME_TYPE_IMG_8, HOME_TYPE_TITLE_8),
+                TypeItem(HOME_TYPE_IMG_9, HOME_TYPE_TITLE_9),
+                TypeItem(HOME_TYPE_IMG_10, HOME_TYPE_TITLE_10),
+                TypeItem(HOME_TYPE_IMG_11, HOME_TYPE_TITLE_11),
+                TypeItem(HOME_TYPE_IMG_12, HOME_TYPE_TITLE_12),
+                TypeItem(HOME_TYPE_IMG_13, HOME_TYPE_TITLE_13),
+                TypeItem(HOME_TYPE_IMG_14, HOME_TYPE_TITLE_14),
+                TypeItem(HOME_TYPE_IMG_15, HOME_TYPE_TITLE_15),
+                TypeItem(HOME_TYPE_IMG_16, HOME_TYPE_TITLE_16))
     }
 
 }
