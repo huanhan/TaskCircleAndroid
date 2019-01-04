c3b52f3ae4c8fa397498b3812e4b330690b4a977
豢涵 <huanhanfu@126.com>
2019-01-04 17:40:38 +0800

    增加登陆UI

--- app/src/main/java/xin/lrvik/taskcicleandroid/ui/fragment/MyFragment.kt
+++ app/src/main/java/xin/lrvik/taskcicleandroid/ui/fragment/MyFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_my.*
+import org.jetbrains.anko.support.v4.startActivity
 import xin.lrvik.taskcicleandroid.R
+import xin.lrvik.taskcicleandroid.baselibrary.ext.onClick
 import xin.lrvik.taskcicleandroid.baselibrary.ui.fragment.BaseFragment
-
+import xin.lrvik.taskcicleandroid.ui.activity.LoginActivity
 
 class MyFragment : BaseFragment() {
 
@@ -15,5 +18,9 @@ class MyFragment : BaseFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        mTvLoginOrRegist.onClick {
+            startActivity<LoginActivity>()
+        }
+
     }
 }
