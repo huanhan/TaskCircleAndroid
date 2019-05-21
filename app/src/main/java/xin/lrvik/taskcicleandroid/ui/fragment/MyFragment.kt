09ec95e9529649f0bf87072bc2a93bff1c4b074e
豢涵 <huanhanfu@126.com>
2019-05-21 18:44:35 +0800

    1.增加关于我们，用户协议提示

--- app/src/main/java/xin/lrvik/taskcicleandroid/ui/fragment/MyFragment.kt
+++ app/src/main/java/xin/lrvik/taskcicleandroid/ui/fragment/MyFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import cn.jpush.android.api.JPushInterface
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_my.*
+import org.jetbrains.anko.alert
 import org.jetbrains.anko.support.v4.alert
 import org.jetbrains.anko.support.v4.startActivity
 import xin.lrvik.taskcicleandroid.R
@@ -151,5 +152,56 @@ class MyFragment : BaseMvpFragment<MyPresenter>(), MyView {
             startActivity<HunterAuditActivity>()
         }
         //mPresenter.detail()
+
+        mRlAbout.onClick {
+            alert("只是为了一个曾经的梦想，我们一直在奋斗！\n" +
+                    "即使我们的作品无人问津，却依然还在坚持！\n" +
+                    "我们不为了成为一名伟大的作家，只想把我们脑子里和心里的那个故事写好！\n" +
+                    "所以无论你是谁，如果看过了我们的作品，请给我们鼓励", "关于我们") {
+                positiveButton("确定") {  }
+            }.show()
+        }
+
+        mRlAgreement.onClick {
+            alert("1、本条所述内容是指用户使用本服务过程中所制作、上载、复制、发布、传播的任何内容，包括但不限于帐号头像、名称、用户说明等注册信息及认证资料，或文字、语音、图片、视频、图文等发送、回复或自动回复消息和相关链接页面，以及其他使用帐号或本服务所产生的内容。\n" +
+                    "\n" +
+                    "2、用户不得利用“任务圈”帐号或本服务制作、上载、复制、发布、传播如下法律、法规和政策禁止的内容：\n" +
+                    "\n" +
+                    "(1) 反对宪法所确定的基本原则的；\n" +
+                    "\n" +
+                    "(2) 危害国家安全，泄露国家秘密，颠覆国家政权，破坏国家统一的；\n" +
+                    "\n" +
+                    "(3) 损害国家荣誉和利益的；\n" +
+                    "\n" +
+                    "(4) 煽动民族仇恨、民族歧视，破坏民族团结的；\n" +
+                    "\n" +
+                    "(5) 破坏国家宗教政策，宣扬邪教和封建迷信的；\n" +
+                    "\n" +
+                    "(6) 散布谣言，扰乱社会秩序，破坏社会稳定的；\n" +
+                    "\n" +
+                    "(7) 散布淫秽、色情、赌博、暴力、凶杀、恐怖或者教唆犯罪的；\n" +
+                    "\n" +
+                    "(8) 侮辱或者诽谤他人，侵害他人合法权益的；\n" +
+                    "\n" +
+                    "(9) 不遵守法律法规底线、社会主义制度底线、国家利益底线、公民合法权益底线、社会公共秩序底线、道德风尚底线和信息真实性底线的“七条底线”要求的；\n" +
+                    "\n" +
+                    "(10) 含有法律、行政法规禁止的其他内容的信息。\n" +
+                    "\n" +
+                    "3、用户不得利用“任务圈”帐号或本服务制作、上载、复制、发布、传播如下干扰“任务圈”正常运营，以及侵犯其他用户或第三方合法权益的内容：\n" +
+                    "\n" +
+                    "(1) 含有任何性或性暗示的；\n" +
+                    "\n" +
+                    "(2) 含有辱骂、恐吓、威胁内容的；\n" +
+                    "\n" +
+                    "(3) 含有骚扰、垃圾广告、恶意信息、诱骗信息的；\n" +
+                    "\n" +
+                    "(4) 涉及他人隐私、个人信息或资料的；\n" +
+                    "\n" +
+                    "(5) 侵害他人名誉权、肖像权、知识产权、商业秘密等合法权利的；\n" +
+                    "\n" +
+                    "(6) 含有其他干扰本服务正常运营和侵犯其他用户或第三方合法权益内容的信息。", "用户协议") {
+                positiveButton("确定") {  }
+            }.show()
+        }
     }
 }
